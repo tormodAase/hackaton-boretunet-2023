a120346c0c9c895d766a8ce4eaa73132c7b1f718
Amund Bogetvedt <amund.engen.bogetvedt@politiet.no>
2023-04-15 18:50:23 +0200

    Lager ProfilePicture komponent.

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,6 @@
 import { View } from "react-native";
 import { BjarteButton } from "../components/BjarteButton";
+import { ProfilePicture } from "../components/ProfilePicture";
 import { Typography } from "../components/Typography";
 
 export default function StartPage() {
@@ -7,5 +8,6 @@ export default function StartPage() {
       <Typography variant="BUTTON">Start Page</Typography>
       <BjarteButton variant="PRIMARY" title="Primary" onPress={()=>{}}></BjarteButton>
       <BjarteButton variant="SECONDARY" title="Secondary" onPress={()=>{}}></BjarteButton>
+      <ProfilePicture source={"https://static.vecteezy.com/system/resources/thumbnails/009/734/564/small/default-avatar-profile-icon-of-social-media-user-vector.jpg"}></ProfilePicture>
     </View>;
 }
