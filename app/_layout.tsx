422635c62871bc9bb31089d939efa168964507ad
Tormod Aase <tormod.aase@gmail.com>
2023-04-15 17:01:37 +0200

    feat: typography

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,4 +1,8 @@
 import { Stack } from "expo-router";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+import { useCallback, useLayoutEffect } from "react";
+import { View } from "react-native";
 
 export const unstable_settings = {
   // Ensure any route can link back to `/`
@@ -6,5 +10,15 @@ export const unstable_settings = {
 };
 
 export default function Layout() {
+  const [fontsLoaded] = useFonts({
+    "VesperLibre-Black": require("../assets/fonts/VesperLibre-Black.ttf"),
+    "VesperLibre-Bold": require("../assets/fonts/VesperLibre-Bold.ttf"),
+    "VesperLibre-Medium": require("../assets/fonts/VesperLibre-Medium.ttf"),
+    "VesperLibre-Regular": require("../assets/fonts/VesperLibre-Regular.ttf"),
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
   return <Stack />;
 }
---
+++ components/Typography.tsx
@@ -0,0 +1,24 @@
+import { Text } from "react-native";
+import { COLORS } from "../colors/colors";
+import { PropsWithChildren } from "react";
+
+type TypographyVariants =
+  | "HEADER_PRIMARY"
+  | "HEADER_SECONDARY"
+  | "HEADER_TERTIARY"
+  | "BUTTON";
+
+export const Typography = (
+  props: PropsWithChildren<{ color?: string; variant: TypographyVariants }>
+) => {
+  let fontSize, color, fontFamily;
+  fontSize = 18;
+  color = COLORS.TEXT_DEFAULT;
+  fontFamily = "VesperLibre-Medium";
+  if (props.variant === "BUTTON") {
+    fontSize = 18;
+    color = COLORS.TEXT_DEFAULT;
+    fontFamily = "VesperLibre-Medium";
+  }
+  return <Text style={{ fontSize, color, fontFamily }}>{props.children}</Text>;
+};
